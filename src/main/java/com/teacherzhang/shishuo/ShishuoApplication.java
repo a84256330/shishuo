db0d4ea4adf016320a87906f31f4795370aeb41e
a84256330 <mc5588zx@126.com>
2019-12-03 16:46:48 +0800

    初始化

---
+++ src/main/java/com/teacherzhang/shishuo/ShishuoApplication.java
@@ -0,0 +1,13 @@
+package com.teacherzhang.shishuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShishuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShishuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teacherzhang/shishuo/ShishuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacherzhang.shishuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShishuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
